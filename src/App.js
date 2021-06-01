2a7bd85d9346cfff83eb3b20c0c5e8f1f6a2ac0f
sean-m-foley <65916610+sean-m-foley@users.noreply.github.com>
2021-06-01 04:25:54 -0500

    Added collection-item component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import './App.css';
-import HomePage from './components/pages/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats!</h1>
-  </div>
-);
-
-const MensPage = () => (
-  <div>
-    <h1>Men's Page</h1>
-  </div>
-);
+import HomePage from './components/pages/homepage/homepage.component';
+import ShopPage from './components/pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
-        <Route path='/shop/mens' component={MensPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch> 
     </div>
   );
--- src/components/pages/homepage/homepage.component.jsx
+++ src/components/pages/homepage/homepage.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Directory from '../directory/directory.component';
+import Directory from '../../directory/directory.component';
 
 import './homepage.styles.scss';
 
 const HomePage = ({history}) => (
    <div className='homepage'>
-       <Directory />
+       <Directory history={history}/>
    </div> 
 )
 
